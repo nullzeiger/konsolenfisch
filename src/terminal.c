439a3e593bf3ef8ffa9f8c00448863461f2d0865
nullzeiger <ivan.guerreschi.dev@gmail>
2025-01-17 17:21:46 +0100

    Add random name for fish

--- src/terminal.c
+++ src/terminal.c
@@ -6,14 +6,20 @@
  * license that can be found in the LICENSE file.
  */
 
+#include <stdlib.h>
+#include <time.h>
 #include "fish.h"
 #include "terminal.h"
 
 /* Returns a greeting string including a fish name. */
 const char *hello_fish(void)
 {
-    /* TODO: Add random name generation. Currently, it always returns "Gold". */
-    const char *name = "Gold";
+    const char *fish_name[5] = {"Goldie", "Finny", "Bubbles", "Sunny", "Splash"};
+    srand(time(NULL));
+    /* Generate a random number between 0 and 4 (inclusive) */
+    int random_number = rand() % 5;
+    const char *name = fish_name[random_number];
+
     return name;
 }
 
